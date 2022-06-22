bfe61d08741cc23a692457d65404ee45982e6b74
thanhtv <tvthanhdl@gmail.com>
2022-06-22 11:47:15 +0700

    update product model convention

--- app/Product.php
+++ app/Product.php
@@ -8,8 +8,16 @@ class Product extends Model
 {
     public $timestamps = false;
     protected $fillable = [
-        'category_id', 'subcategory_id', 'product_name', 'product_quantity', 'product_sold', 'product_desc',
-        'product_content', 'product_price', 'product_image', 'product_status'
+	    'category_id',
+	    'subcategory_id',
+	    'product_name',
+	    'product_quantity',
+	    'product_sold',
+	    'product_desc',
+            'product_content',
+	    'product_price',
+	    'product_image',
+	    'product_status'
     ];
     protected $primaryKey = 'product_id';
     protected $table = 'tbl_product';
