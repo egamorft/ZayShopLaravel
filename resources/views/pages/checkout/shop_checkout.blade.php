eff97fb7ecf55300c31e1d1de2f5d377034a138e
Huy Tung <huytung.htbn@gmail.com>
2022-06-22 09:39:57 +0700

    Create modal for customers to check their bill after confirm order

--- resources/views/pages/checkout/shop_checkout.blade.php
+++ resources/views/pages/checkout/shop_checkout.blade.php
@@ -39,7 +39,282 @@ Session::put('error', null);
 @endif
 <!-- End Alert -->
 
+<?php
+$content = Cart::content();
 
+$tax = Cart::pricetotal(0, ',', '') * 10 / 100;
+
+$total_all = Cart::total(0, ',', '') + Session::get('fee');
+?>
+<div class="modal fade" id="myModal">
+    <div class="modal-dialog modal-xl">
+        <div class="modal-content">
+            <div class="container">
+                <div class="row d-flex justify-content-center">
+                    <div class="col-md-8">
+                        <div class="card">
+                            <div class="text-left logo p-2 px-5">
+
+                                <a class="navbar-brand text-success logo h1 align-self-center" href="{{URL::to('/')}}">
+                                    Zay Shop
+                                </a>
+
+
+                            </div>
+                            <h3 class="text-left logo p-2 px-5" id="countdown"></h3>
+                            <div class="invoice p-5">
+
+                                <h5>Your order is now Confirmed!</h5>
+
+                                <span class="font-weight-bold d-block mt-4">Hello, <?php echo Session::get('account_name'); ?></span>
+                                <span>You order has been confirmed and will coming to you as soon as possible!</span>
+
+                                <div class="payment border-top mt-3 mb-3 border-bottom table-responsive">
+
+                                    <table class="table table-borderless">
+
+                                        <tbody>
+                                            <tr>
+                                                <td>
+                                                    <div class="py-2">
+
+                                                        <span class="d-block text-muted">Order Date</span>
+                                                        <span>12 Jan,2018</span>
+
+                                                    </div>
+                                                </td>
+
+                                                <td>
+                                                    <div class="py-2">
+
+                                                        <span class="d-block text-muted">Order No</span>
+                                                        <span>MT12332345</span>
+
+                                                    </div>
+                                                </td>
+
+                                                <td>
+                                                    <div class="py-2">
+
+                                                        <span class="d-block text-muted">Payment</span>
+                                                        <span><img src="https://img.icons8.com/color/48/000000/mastercard.png" width="20" /></span>
+
+                                                    </div>
+                                                </td>
+
+                                                <td>
+                                                    <div class="py-2">
+
+                                                        <span class="d-block text-muted">Shiping Address</span>
+                                                        <span>414 Advert Avenue, NY,USA</span>
+
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                        </tbody>
+
+                                    </table>
+
+
+
+
+
+                                </div>
+
+
+
+
+                                <div class="product border-bottom table-responsive">
+
+                                    <table class="table table-borderless">
+
+                                        <tbody>
+                                            @foreach($content as $con)
+                                            <tr>
+                                                <td width="20%">
+
+                                                    <img src="{{URL::to('/public/upload/product/'.$con->options->image)}}" width="90px">
+
+                                                </td>
+
+                                                <td width="60%">
+                                                    <span class="font-weight-bold">{{$con->name}}</span>
+                                                    <div class="product-qty">
+                                                        <small class="text-muted">Quantity:{{$con->qty}}</small>
+
+                                                    </div>
+                                                </td>
+                                                <td width="20%">
+                                                    <div class="text-right">
+                                                        <span class="font-weight-bold">{{$con->price(0 , ',' , '.').' '.'đ'}}</span>
+                                                    </div>
+                                                </td>
+                                            </tr>
+                                            @endforeach
+                                        </tbody>
+
+                                    </table>
+
+
+
+                                </div>
+
+
+
+                                <div class="row d-flex justify-content-end">
+
+                                    <div class="col-md-5">
+
+                                        <table class="table table-borderless">
+
+                                            <tbody class="totals">
+
+                                                <tr>
+                                                    <td>
+                                                        <div class="text-left">
+
+                                                            <span class="text-muted">Subtotal</span>
+
+                                                        </div>
+                                                    </td>
+                                                    <td>
+                                                        <div class="text-right">
+                                                            <span>{{Cart::pricetotal(0 , ',' , '.').' '.'đ'}}</span>
+                                                        </div>
+                                                    </td>
+                                                </tr>
+
+                                                @if(Session::get('fee'))
+                                                <tr>
+                                                    <td>
+                                                        <div class="text-left">
+
+                                                            <span class="text-muted">Shipping Fee</span>
+
+                                                        </div>
+                                                    </td>
+                                                    <td>
+                                                        <div class="text-right">
+                                                            <span class="text-danger">{{number_format(Session::get('fee'), 0 , ',' , '.')}} đ</span>
+                                                        </div>
+                                                    </td>
+                                                </tr>
+                                                @else
+                                                <tr>
+                                                    <td>
+                                                        <div class="text-left">
+
+                                                            <span class="text-muted">Shipping Fee</span>
+
+                                                        </div>
+                                                    </td>
+                                                    <td>
+                                                        <div class="text-right">
+                                                            <span class="text-danger">0 đ</span>
+                                                        </div>
+                                                    </td>
+                                                </tr>
+                                                @endif
+
+
+                                                <tr>
+                                                    <td>
+                                                        <div class="text-left">
+
+                                                            <span class="text-muted">Tax Fee</span>
+
+                                                        </div>
+                                                    </td>
+                                                    <td>
+                                                        <div class="text-right">
+                                                            <span class="text-danger">{{number_format($tax, 0, ',', '.').' '.'đ'}}</span>
+                                                        </div>
+                                                    </td>
+                                                </tr>
+
+
+                                                <tr>
+                                                    <td>
+                                                        <div class="text-left">
+
+                                                            <span class="text-muted">Discount</span>
+
+                                                        </div>
+                                                    </td>
+                                                    <td>
+                                                        <div class="text-right">
+                                                            <span class="text-success">{{Cart::discount(0 , ',' , '.').' '.'đ'}}</span>
+                                                        </div>
+                                                    </td>
+                                                </tr>
+
+
+                                                <tr class="border-top border-bottom">
+                                                    <td>
+                                                        <div class="text-left">
+
+                                                            <span class="font-weight-bold">Subtotal</span>
+
+                                                        </div>
+                                                    </td>
+                                                    <td>
+                                                        <div class="text-right">
+                                                            <span class="font-weight-bold">{{number_format($total_all, 0 , ',' , '.')}} đ</span>
+                                                        </div>
+                                                    </td>
+                                                </tr>
+
+                                            </tbody>
+
+                                        </table>
+
+                                    </div>
+
+
+
+                                </div>
+
+
+                                <p>We will be sending shipping confirmation email when the item shipped successfully!</p>
+                                <p class="font-weight-bold mb-0">Thanks for shopping with us!</p>
+                                <span>Zay Shop Team</span>
+
+
+
+
+
+                            </div>
+
+
+                            <div class="d-flex justify-content-between footer p-3">
+
+                                <span>Need Help? visit our <a href="#"> help center</a></span>
+                                @php
+                                $orderDate = Carbon\Carbon::now();
+                                echo $orderDate->format('d/m/Y');
+                                @endphp
+
+                            </div>
+
+
+
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+</div>
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#myModal">
+    Open modal
+</button>
+
+
+<!--Check out content -->
 <main class="container">
     <div class="py-5 text-center">
         <h2>Checkout form</h2>
@@ -101,9 +376,6 @@ Session::put('error', null);
                 <span class="badge bg-primary rounded-pill">{{Cart::count()}}</span>
             </h4>
             <ul class="list-group mb-3">
-                <?php
-                $content = Cart::content();
-                ?>
                 @foreach($content as $value)
                 <li class="list-group-item d-flex justify-content-between lh-sm">
                     <div>
@@ -117,9 +389,6 @@ Session::put('error', null);
                     <span>Sub Total</span>
                     <strong>{{Cart::pricetotal(0 , ',' , '.').' '.'đ'}}</strong>
                 </li>
-                @php
-                $tax = Cart::pricetotal(0 , ',' , '') * 10 / 100;
-                @endphp
                 <li class="list-group-item d-flex justify-content-between bg-light">
                     <div class="text-danger">
                         <h6 class="my-0">Tax</h6>
